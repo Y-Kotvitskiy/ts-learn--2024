5b1ead30d8c8b2929672ec3a60f64963ede323fd
Yevgen Kotvitskyi <yevgeniy.kotvitskyi@gmail.com>
2024-09-19 00:14:44 +0300

    homework-02, v1

--- src/homework-02/basic_literal.ts
+++ src/homework-02/basic_literal.ts
@@ -10,7 +10,6 @@ type Lecturer = {
 
 type Areas = Area[];
 type Lecturers = Lecturer[];
-type Levels = Level[];
 
 class School {
   // implement 'add area', 'remove area', 'add lecturer', and 'remove lecturer' methods
@@ -25,7 +24,43 @@ class School {
   get lecturers(): Lecturers {
     return this._lecturers;
   }
+
+  addArea(value: Area): boolean {
+    if (this._areas.find(x => x.name === value.name)) {
+      return false;
+    }
+    this._areas.push(value);
+    return true;
+  }
+
+  removeArea(value: Area): boolean {
+    const index = this._areas.findLastIndex(x => x.name === value.name);
+    if (index < 0) {
+      return false;
+    }
+    this._areas.splice(index, 1);
+    return true;
+  }
+
+  addLecturer(value: Lecturer): boolean {
+    if (this._lecturers.find(x => x.name === value.name)) {
+      return false;
+    }
+    this._lecturers.push(value);
+    return true;
+  }
+
+  removeLecturer(value: Lecturer): boolean {
+    const index = this._lecturers.findLastIndex(x => x.name === value.name);
+    if (index < 0) {
+      return false;
+    }
+    this._lecturers.splice(index, 1);
+    return true;
+  }
 }
+
+type Levels = Level[];
 
 class Area {
   // implement getters for fields and 'add/remove level' methods
@@ -35,31 +70,143 @@ class Area {
   constructor(name: string) {
     this._name = name;
   }
+
+  get levels(): Levels {
+    return this._levels;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  addLevel(value: Level): boolean {
+    if (this._levels.find(x => x.name === value.name)) {
+      return false;
+    }
+    this._levels.push(value);
+    return true;
+  }
+
+  removeLevel(value: Level): boolean {
+    const index = this._levels.findLastIndex(x => x.name === value.name);
+    if (index < 0) {
+      return false;
+    }
+    this._levels.splice(index, 1);
+    return true;
+  }
 }
+
+type Groups = Group[];
+
+type SameGroup = (a: Group, b: Group) => boolean;
+
+const sameGrop: SameGroup = (a: Group, b: Group) => {
+  return a.directionName === b.directionName && a.levelName === b.levelName;
+};
 
 class Level {
   // implement getters for fields and 'add/remove group' methods
 
-  _groups;
-  _name;
+  _groups: Groups = [];
+  _name: string;
+  _description: string;
 
-  constructor(name, description) {
-    this.name = name;
+  constructor(name: string, description: string) {
+    this._name = name;
     this._description = description;
+  }
+
+  get groups(): Groups {
+    return this._groups;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get description(): string {
+    return this._description;
+  }
+
+  addGroup(value: Group): boolean {
+    if (this._groups.find(x => sameGrop(x, value))) {
+      return false;
+    }
+    this._groups.push(value);
+    return true;
+  }
+
+  removeGroup(value: Group): boolean {
+    const index = this._groups.findIndex(x => sameGrop(x, value));
+    if (index < 0) {
+      return false;
+    }
+    this._groups.splice(index, 1);
+    return true;
   }
 }
 
+type Students = Student[];
+
+type SameStudent = (a: Student, b: Student) => boolean;
+
+const sameStudent: SameStudent = (a: Student, b: Student) => {
+  return a.fullName === b.fullName && a.age === b.age;
+};
+
 class Group {
   // implement getters for fields and 'add/remove student' and 'set status' methods
-  _directionName: string; 
+  _directionName: string;
   _levelName: string;
-  _area?: Area ;
-  _status?: string;
+  _area?: Area;
+  _status: string = '';
   _students: Student[] = []; // Modify the array so that it has a valid toSorted method*
 
   constructor(directionName: string, levelName: string) {
     this._directionName = directionName;
     this._levelName = levelName;
+  }
+
+  get directionName(): string {
+    return this._directionName;
+  }
+
+  get levelName(): string {
+    return this._levelName;
+  }
+
+  get area(): Area | undefined {
+    return this._area;
+  }
+
+  get status(): string {
+    return this._status;
+  }
+
+  set status(value: string) {
+    this._status = value;
+  }
+
+  get students(): Students {
+    return this._students;
+  }
+
+  addStudent(value: Student): boolean {
+    if (this._students.find(x => sameStudent(x, value))) {
+      return false;
+    }
+    this._students.push(value);
+    return true;
+  }
+
+  removeStudent(value: Student): boolean {
+    const index = this._students.findIndex(x => sameStudent(x, value));
+    if (index < 0) {
+      return false;
+    }
+    this._students.splice(index, 1);
+    return true;
   }
 
   showPerformance() {
@@ -68,8 +215,8 @@ class Group {
   }
 }
 
-type Grade = { workname: string; mark: number };
-type Visit = { lesson: string; present: boolean };
+type Grade = [string, number];
+type Visit = [string, boolean];
 
 class Student {
   // implement 'set grade' and 'set visit' methods
@@ -77,8 +224,8 @@ class Student {
   _firstName: string;
   _lastName: string;
   _birthYear: number;
-  _grades: Grade[] = []; // workName: mark
-  _visits: Visit[] = []; // lesson: present
+  _grades: Grade[] = []; // workName: mark [string, number]
+  _visits: Visit[] = []; // lesson: present [string, boolean]
 
   constructor(firstName: string, lastName: string, birthYear: number) {
     this._firstName = firstName;
@@ -99,18 +246,18 @@ class Student {
   }
 
   setGrade(grade: Grade): void {
-    const lastGrade: Grade | undefined = this._grades.find(currGrade => currGrade.workname === grade.workname);
+    const lastGrade: Grade | undefined = this._grades.find(currGrade => currGrade[0] === grade[0]);
     if (lastGrade) {
-      lastGrade.mark = grade.mark;
+      lastGrade[1] = grade[1];
     } else {
       this._grades.push(grade);
     }
   }
 
   setVisit(visit: Visit): void {
-    const lastVisit: Visit | undefined = this._visits.find(currVisit => currVisit.lesson === visit.lesson);
+    const lastVisit: Visit | undefined = this._visits.find(currVisit => currVisit[0] === visit[0]);
     if (lastVisit) {
-      lastVisit.present = visit.present;
+      lastVisit[1] = visit[1];
     } else {
       this._visits.push(visit);
     }
@@ -121,7 +268,7 @@ class Student {
 
     if (!gradeValues.length) return 0;
 
-    const averageGrade = gradeValues.reduce((sum, grade) => sum + grade.mark, 0) / gradeValues.length;
+    const averageGrade = gradeValues.reduce((sum, grade) => sum + grade[1], 0) / gradeValues.length;
     const attendancePercentage = (this._visits.filter(present => present).length / this._visits.length) * 100;
 
     return (averageGrade + attendancePercentage) / 2;
